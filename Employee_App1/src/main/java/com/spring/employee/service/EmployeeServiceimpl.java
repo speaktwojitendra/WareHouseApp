0d610d044e6031fa11fe7423cd40955b059e43ac
jitendra <speaktwojitendra@gmail.com>
2026-01-15 15:35:11 +0530

    modified

--- Employee_App1/src/main/java/com/spring/employee/service/EmployeeServiceimpl.java
+++ Employee_App1/src/main/java/com/spring/employee/service/EmployeeServiceimpl.java
@@ -16,7 +16,7 @@ public class EmployeeServiceimpl implements Iemployee_Service {
 	@Override
 	public Integer seveDetails(Employee e) {
 		// TODO Auto-generated method stub
-		int j=10;
+
 		Employee emp = repo.save(e);
 		return emp.getId();
 	}
